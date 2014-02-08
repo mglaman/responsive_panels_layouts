def1078b2f887ebb2a617662e480853c2d150942
Matt Glaman <nmd.matt@gmail.com>
2014-02-08 16:34:17 -0600

    Setup base module files and Compass config

---
+++ config.rb
@@ -0,0 +1,15 @@
+# Require any additional compass plugins here.
+require 'toolkit'
+
+# Set this to the root of your project when deployed:
+css_dir = "panels"
+sass_dir = "sass"
+
+# You can select your preferred output style here (can be overridden via the command line):
+ output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+ line_comments = false
